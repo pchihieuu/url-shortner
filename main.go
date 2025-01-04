de2436f625d55b7274fd1b27bee5a3e6e1ace1b6
Hieu Phan <hieup3611@gmail.com>
2025-01-04 20:02:09 +0700

    feat: core features

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/pchihieuu/url-shortner.git/api/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err!= nil {
+        fmt.Println(err)
+    }
+	
+	router := gin.Default()
+	setupRouters(router)
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8081"
+	}
+
+	log.Fatal(router.Run(":" + port))
+}
+
+func setupRouters(router *gin.Engine) {
+	router.POST("/api/v1", routes.ShortenURL)
+}
